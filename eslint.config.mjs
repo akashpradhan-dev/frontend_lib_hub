b331ad50c159d31c8294b6bb6e9ab4dfeac2b5bc
Akash Pradhan <akashpradhan151@gmail.com>
2025-09-21 00:23:39 +0530

    fixed build issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ['.next/', '.vscode', 'node_modules/'],
+    ignores: ['.next/', '.vscode', 'node_modules/', 'next-env.d.ts'],
   },
   ...compat.config({
     extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
