90280b5e374dae0e718c3c8a1273fc94e2e8c3d8
Akash Pradhan <akashpradhan151@gmail.com>
2025-09-21 22:31:56 +0530

    fixed

--- src/app/(auth)/oauth-success/page.tsx
+++ src/app/(auth)/oauth-success/page.tsx
@@ -1,42 +1,7 @@
-'use client'
+import OAuthSuccessPage from '@/components/OAuthSuccess'
+import React from 'react'
 
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useAuth } from '@/contexts/AuthContext'
-import toast from 'react-hot-toast'
-import { useMeQuery } from '@/services/query/useMeQuery'
-import { useEffect } from 'react'
-import Cookies from 'js-cookie'
-
-export default function OAuthSuccessPage() {
-  const router = useRouter()
-  const { login } = useAuth()
-  const param = useSearchParams()
-  const token = param.get('token')
-
-  // Store token immediately so TanStack Query can use it
-  if (token) {
-    Cookies.set('token', token, { expires: 7 })
-  }
-
-  const { data, status, error } = useMeQuery({
-    enabled: !!token,
-  })
-
-  useEffect(() => {
-    if (status === 'success' && data?.status === 'success') {
-      const user = data.data
-      login(user) // ✅ safe here
-      toast.success('Google login successful!', { id: 'login_success' })
-      router.replace(
-        user.role === 'admin' ? '/admin/dashboard' : '/user/profile',
-      )
-    } else if (status === 'error') {
-      toast.error('Google login failed. Redirecting to login.')
-      console.error('OAuth login failed:', error?.message)
-      router.replace('/login')
-    }
-  }, [status, data, error, login, router])
-
+function success() {
   return (
     <section className="flex items-center justify-center min-h-[90vh] ">
       <div className="bg-white/5 backdrop-blur-md rounded-3xl shadow-lg p-12 text-center max-w-md w-full animate-fadeIn">
@@ -46,14 +11,10 @@ export default function OAuthSuccessPage() {
         <p className="text-white/70 text-base md:text-lg mb-8">
           Connecting your Google account. Please wait a moment.
         </p>
-        <div className="flex justify-center mb-6">
-          <div className="size-12 border-4 border-white/20 border-t-white rounded-full animate-spin"></div>
-        </div>
-        <div className="text-white/50 text-sm md:text-base">
-          {status === 'pending' && 'Authenticating...'}
-          {status === 'error' && 'Something went wrong. Redirecting you...'}
-        </div>
+        <OAuthSuccessPage />
       </div>
     </section>
   )
 }
+
+export default success
